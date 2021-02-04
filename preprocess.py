4dc679f4616fadc0be8c6894bf121fef64adc8f8
yair_zohav <yairzohav@gmail.com>
2021-02-04 19:48:12 +0200

    env wrapper added and fixed

--- preprocess.py
+++ preprocess.py
@@ -2,5 +2,5 @@ import torch
 
 
 def state_process(state):
-    state = torch.FloatTensor(state).permute(2, 0, 1)/255
+    state = torch.FloatTensor(state) #.permute(2, 0, 1)/255
     return state.unsqueeze(0)
